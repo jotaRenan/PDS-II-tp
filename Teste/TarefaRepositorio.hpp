fa3a54b1ad055a17c874824abcefd897fbb8abe2
Joao Pedro Rosa <joao.rosa@dtidigital.com.br>
2018-11-10 12:53:52 -0200

    Define TarefaRepositorio.hpp

--- Teste/TarefaRepositorio.hpp
+++ Teste/TarefaRepositorio.hpp
@@ -1,6 +1,14 @@
 #pragma once
 #include "RepositorioBase.h"
 #include "Tarefa.hpp"
-class TarefaRepositorio : public RepositorioBase<Tarefa> {
 
+class TarefaRepositorio 
+	: public RepositorioBase<Tarefa> {
+	
+	// Herdado por meio de RepositorioBase
+	virtual bool inserir(Tarefa) override;
+	virtual bool atualizar(Tarefa) override;
+	virtual Tarefa remover(long) override;
+	virtual Tarefa obter(long) override;
+	virtual std::vector<Tarefa> obter() override;
 };
