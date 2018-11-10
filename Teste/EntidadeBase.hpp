b2c9db76ac8e9828f12e66777736d1a7efcc041c
Joao Pedro Rosa <joao.rosa@dtidigital.com.br>
2018-11-10 11:53:20 -0200

    Define EntidadeBase.hpp

---
+++ Teste/EntidadeBase.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "Serializavel.hpp"
+
+class EntidadeBase: public Serializavel {
+	public:
+		long id;
+};
---
+++ Teste/RepositorioBase.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+template<class T> class RepositorioBase {
+	public:
+		virtual bool inserir(T) = 0;
+		virtual bool atualizar(T) = 0;
+		virtual T remover(long) = 0;
+		virtual T obter(long) = 0;
+		virtual std::vector<T> obter() = 0;
+};
